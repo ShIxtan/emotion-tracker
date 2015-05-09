c505b2052b6c4e895aeebf59ffad673a2ee84819
ShIxtan <aulbrich@iu.edu>
2015-05-09 16:41:32 -0700

    add face tracking libraries

--- popup.js
+++ popup.js
@@ -28,4 +28,62 @@ document.addEventListener('DOMContentLoaded', function() {
   getCurrentTab(function(tab) {
     renderStatus('currently on: ' + tab.title);
   });
+
+  // *** this code is for video recording ***
+  var vid = document.getElementById('videoel');
+  var overlay = document.getElementById('overlay');
+	var overlayCC = overlay.getContext('2d');
+
+  // check for camerasupport
+  if (navigator.webkitGetUserMedia) {
+    // set up stream
+
+    var videoSelector = {video : true};
+
+    navigator.webkitGetUserMedia(videoSelector, function( stream ) {
+      vid.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+      vid.play();
+    }, function() {
+      chrome.runtime.openOptionsPage();
+      alert("There was some problem trying to fetch video from your webcam. If you have a webcam, please make sure to accept when the browser asks for access to your webcam.");
+    });
+  } else {
+    alert("This extension depends on getUserMedia, which your browser does not seem to support. :(");
+  }
+
+  /*********** setup of emotion detection *************/
+
+  var ctrack = new clm.tracker({useWebGL : true});
+
+  ctrack.init(pModel);
+  var ec = new emotionClassifier();
+  ec.init(emotionModel);
+  var emotionData = ec.getBlank();
+
+  function startTracking() {
+    vid.play();
+    ctrack.start(vid);
+    loop = setInterval(drawLoop.bind(this), 100);
+
+    return loop;
+  }
+
+  function drawLoop() {
+    overlayCC.clearRect(0, 0, 400, 300);
+    if (ctrack.getCurrentPosition()) {
+      ctrack.draw(overlay);
+    }
+    var cp = ctrack.getCurrentParameters();
+
+    var er = ec.meanPredict(cp);
+    if (er) {
+      for (i in er){
+        if (er[i].value > 0.5){
+          console.log(er[i].emotion + ": " + er[i].value)
+        }
+      }
+    }
+  }
+
+  startTracking();
 });
